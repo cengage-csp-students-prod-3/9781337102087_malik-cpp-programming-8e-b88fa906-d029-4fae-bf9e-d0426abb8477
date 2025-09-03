2bf7da2d42adc0bf9da8393275759a39f5939478
BaoanD <baoanduong@dcc.edu>
2025-09-03 23:57:40 +0000

    Testing Wed Sep 03 2025 23:57:40 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex04/student/main.cpp
+++ chapter3/ex04/student/main.cpp
@@ -9,28 +9,33 @@ int main()
     double cost;
     double area;
     double bagSize;
-    
+
     cout << fixed << showpoint << setprecision(2);
-    
+
     cout << "Enter the amount of fertilizer, in pounds, "
-         << "in one bag: ";
+        << "in one bag: ";
     cin >> bagSize;
     cout << endl;
-    
-    cout << "Enter the cost of the " << bagSize
-         << " pound fertilizer bag: ";
+
+    cout << "Enter the cost in dollar of the " << bagSize
+        << " pound fertilizer bag: ";
     cin >> cost;
     cout << endl;
-    
+
     cout << "Enter the area, in square feet, that can be "
-         << "fertilized by one bag: ";
+        << "fertilized by one bag: ";
     cin >> area;
     cout << endl;
-    
+
     cout << "The cost of the fertilizer per pound is: $"
-         << bagSize / cost << endl;
-    
+        << cost / bagSize << endl;
+
+    cout << endl;
+
     cout << "The cost of fertilizing per square foot is: $"
-         << area / cost << endl;
+        << cost / area << endl;
+
+    cout << endl;
+
     return 0;
 }
