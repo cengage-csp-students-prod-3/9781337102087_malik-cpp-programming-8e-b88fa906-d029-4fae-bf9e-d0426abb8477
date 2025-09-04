bd91abedd83f760c7eabb6af4e308a1797f28dfb
BaoanD <baoanduong@dcc.edu>
2025-09-04 23:59:03 +0000

    Testing Thu Sep 04 2025 23:59:03 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex03/student/main.cpp
+++ chapter3/ex03/student/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     // Write your main here
     double kg = 0.0;
-    double ibs = 0.0;
+    double lbs = 0.0;
     const double KG_TO_LBS_CONVERSION = 2.2;
 
     // Prompt user to enter weight in kilogram 
@@ -13,10 +13,10 @@ int main() {
     cout << "Enter your weight in kilograms." << endl;
     cin >> kg;
 
-    ibs = KG_TO_LBS_CONVERSION * kg;
+    lbs = KG_TO_LBS_CONVERSION * kg;
 
     // Display the person's weight in pounds
 
-    cout << "Your weight is " << ibs << " lbs" << endl;
+    cout << "Your weight is " << lbs << " lbs" << endl;
     return 0;
 }
