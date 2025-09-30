62927c984dd5d3ac154d2c5d07129e8e4d8b1f0b
BaoanD <baoanduong@dcc.edu>
2025-09-30 21:08:28 +0000

    Testing Tue Sep 30 2025 21:08:28 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex16/student/main.cpp
+++ chapter2/ex16/student/main.cpp
@@ -4,5 +4,31 @@ using namespace std;
 
 int main() {
     // Write your main here
+
+    const double milkPerCarton = 3.78; // liters
+    const double costPerLiter = 0.38;
+    const double profitPerCarton = 0.27;
+
+    double litersOfMilk;
+    int numOfCartons;
+    double costOfProducingMilk;
+    double profitOfProducingMilk;
+
+    // Prompt user to enter the total amount of milk produced in the morning
+
+    cout << "Enter the amount of milk produced in the morning in liters: ";
+    cin >> litersOfMilk;
+
+    // Calculate the number of cartons needed, cost of producing milk,
+    // and profit from producing milk
+
+    numOfCartons = static_cast<int>(litersOfMilk / milkPerCarton);
+    costOfProducingMilk = litersOfMilk * costPerLiter;
+    profitOfProducingMilk = numOfCartons * profitPerCarton;
+
+    cout << "Number of cartons needed: " << numOfCartons << endl;
+    cout << "Cost of producing milk: $" << costOfProducingMilk << endl;
+    cout << "Profit from producing milk: $" << profitOfProducingMilk << endl;
+
     return 0;
 }
