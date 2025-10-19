e42c1ed82503c3a361b4ced14b4c8e6ed5513f54
BaoanD <baoanduong@dcc.edu>
2025-10-19 17:19:50 +0000

    Testing Sun Oct 19 2025 17:19:50 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex05/student/main.cpp
+++ chapter8/ex05/student/main.cpp
@@ -1,8 +1,36 @@
+// This program prompts the user to input a string and outputs the string in uppercase letters.
+// Date: 10/19/2025
+
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
 int main() {
     // Write your main here
+    string inputString;
+    string upperCaseString;
+    int stringLength = 0;
+
+    // Prompt user to enter a string
+    cout << "Enter a string" << endl;
+    cin >> inputString;
+
+    // Convert the string to uppercase
+
+    stringLength = inputString.length();
+
+    for(int index = 0; index < stringLength; index++) {
+        upperCaseString[index] = toupper(inputString[index]);
+    }
+
+    // Output the string in uppercase letters
+
+    cout << "The string in uppercase letters is: ";
+    for(int x = 0; x < stringLength; x++) {
+        cout << upperCaseString[x];
+    }
+    cout << endl;
+
     return 0;
 }
