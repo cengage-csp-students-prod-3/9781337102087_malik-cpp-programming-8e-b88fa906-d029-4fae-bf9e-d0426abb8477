39b58c2594f27ca91042722695fe8cc2bc8dd13a
BaoanD <baoanduong@dcc.edu>
2025-11-09 21:36:47 +0000

    Testing Sun Nov 09 2025 21:36:47 GMT+0000 (Coordinated Universal Time)

--- chapter14/ex01/student/main.cpp
+++ chapter14/ex01/student/main.cpp
@@ -1,8 +1,36 @@
+// This program converts the length entered from inches to centimeters.
+// 11/9/2025
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
     // Write your main here
+
+    const double centimeterConversion = 2.54;
+
+    int length;
+    double convertedLength;
+
+    try 
+    {
+        // Prompt user to enter length in feet and in inches
+
+        cout << "Enter the length in feet" << endl;
+        cin >> length;
+
+        if(cin.fail()) {
+            throw length;
+        }
+
+        convertedLength = length * centimeterConversion;
+
+        cout << "The length in centimeters is: " << convertedLength;
+    }
+
+    catch (int x) {
+        cout << "A non positive number is entered" << endl;
+    }
     return 0;
 }
