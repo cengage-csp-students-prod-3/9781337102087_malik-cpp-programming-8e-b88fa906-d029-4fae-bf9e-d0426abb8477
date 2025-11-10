bed033f5549c93e644e7340b98da2c3d001915eb
BaoanD <baoanduong@dcc.edu>
2025-11-10 16:58:04 +0000

    Testing Mon Nov 10 2025 16:58:04 GMT+0000 (Coordinated Universal Time)

--- chapter14/ex04/student/main.cpp
+++ chapter14/ex04/student/main.cpp
@@ -1,8 +1,27 @@
+// This program prompts the user to enter time in 12-hour notation. The program
+// then outputs the time in 24-hour notation.
+// 11/10/2025
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
     // Write your main here
+
+    int inputTime;
+    int outputNotation;
+
+    //try {
+        // Prompt user to enter time
+
+        cout << "Enter the time in 12-hour notation" << endl;
+        cin >> inputTime;
+        cout << endl;
+
+        outputNotation = inputTime + 12;
+
+        cout << "The time in 24-hour notation is " << outputNotation << endl;
+    //}
     return 0;
 }
