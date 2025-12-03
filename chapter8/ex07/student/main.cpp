8ff16558d3c4f8e3014cffe76607afdbda7bc77d
BaoanD <baoanduong@dcc.edu>
2025-12-03 20:34:47 +0000

    Testing Wed Dec 03 2025 20:34:47 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex07/student/main.cpp
+++ chapter8/ex07/student/main.cpp
@@ -21,9 +21,7 @@ int main() {
     double percentOfTotalVotes[NUM_OF_CANDIDATES];
     int totalVotes = 0;
 
-    cout << fixed << showpoint;
-
-    cout << setprecision(2);
+    //cout << fixed << showpoint;
 
     // Prompt user to enter the input the last name of the five candidates 
     // and the number of votes received by each candidate.
@@ -41,8 +39,10 @@ int main() {
     // Calculate the percentage of the total votes received by the candidate
 
     for (int i = 0; i < NUM_OF_CANDIDATES; i++) {
+        cout << "Number in double is "<< static_cast<double>(totalVotes);
         percentOfTotalVotes[i] = votesReceived[i] / static_cast<double>(totalVotes);
-        //cout << "percentage of total votes is " << percentOfTotalVotes[i] << endl;
+        percentOfTotalVotes[i] = percentOfTotalVotes[i] * 100;
+        cout << "percentage of total votes is " << percentOfTotalVotes[i] << endl;
     }
 
     /*for(int x = 0; x < 5; x++) {
@@ -74,6 +74,8 @@ void outputWinnerOfElection(string candidate[], int votes[], double percentOfVot
     string winner;
     int largest_element_index = 0;
     
+    cout << setprecision(4);
+
     // Find the largest element in the votes array
 
     int largest_element = votes[0];
